2b431a2f486e6cc3fb2fd684d5d4242be0612061
danishahsancs <danishahsancs@gmail.com>
2025-10-05 20:05:42 -0400

    26

---
+++ 26.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int i = 0;
+        for (int num : nums) {
+            if (!set.contains(num)) {
+                set.add(num);
+                nums[i] = num;
+                i++;
+            }
+        }
+
+        return i;
+    }
+}
