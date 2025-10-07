e9ce3d077deebe7daeda0c676b8d39f33f22f0a2
danishahsancs <danishahsancs@gmail.com>
2025-10-07 08:56:48 -0400

    27&28

---
+++ 28.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0) && i + needle.length() <= haystack.length()) {
+                if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                    return i;
+                }
+
+            }
+        }
+
+        return -1;
+    }
+}
